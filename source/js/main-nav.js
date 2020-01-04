2c6240b0e9aa5c7c29ada5e391290e4503206c3c
Юлия Корнейчук <uliakorneychuk@MacBook-Air-Ulia.local>
2020-01-04 16:37:34 +0300

    Вносит правки в стилизацию

---
+++ source/js/main-nav.js
@@ -0,0 +1,11 @@
+<script>
+      var navMain = document.querySelector('.main-nav');
+      var navToggle = document.querySelector('.page-header__toggle');
+
+      navMain.classList.remove('main-nav--nojs');
+      navToggle.removeAttribute('disabled')
+
+      navToggle.addEventListener('click', function() {
+        navMain.classList.toggle('main-nav--closed');
+      });
+</script>
